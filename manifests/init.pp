b68e5dba257a9078d8486ebae84db6f54a3ce2db
Saurabh D <saurabh.dikshit@puppet.com>
2023-03-06 13:10:54 +0000

    sdssfdewtwete

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,4 @@
 class teyaml {
-# resources
-$deferred_file = Deferred('deferred_file', [ '/etc/hostname' ])
-$deferred_content = Deferred('inline_epp', ['Result: <%= $var %>', { 'var' => $deferred_file }])
-
-notify { 'test message':
-message => $deferred_content
-}
-
-file { '/root/result.txt':
-ensure => file,
-content => $deferred_content
-}
-
 file { '/tmp/file1':
   ensure  => file,
   content => 'Hello',
